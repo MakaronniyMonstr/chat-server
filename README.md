0cc7905e093d64c1b77a18e961d73300c135c1ff
MakaronniyMonstr <57591579+MakaronniyMonstr@users.noreply.github.com>
2021-09-29 22:53:01 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+#Resource server based on Java Spring
+
+### Tasks
+
+```
+Refresh/access token based authentication +
+Role based access +
+WebSocket based messaging -
+Full cover tests -
+Docker containerization -
+```
--- src/main/java/com/vesko/chatserver/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/vesko/chatserver/filter/JwtAuthenticationFilter.java
@@ -19,7 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Slf4j
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final ITokenBoxService tokenBoxService;
